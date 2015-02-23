301138abf6db4dd40044bfd922f3bd4d7c26f2af
Joe Keohah <jkeohan@gmail.com>
2015-02-23 18:18:14 -0500

    updated world map

--- public/javascripts/worldmap.js
+++ public/javascripts/worldmap.js
@@ -2,7 +2,10 @@ var width = 800;
 var height = 375;
 var map = void 0; // Update global
 
-var color = d3.scale.category20c()
+//var color = d3.scale.category20c()
+var color = d3.scale.ordinal()
+  .domain(["Full","Partial: CMT 1","Partial: CMT 2","Partial: Training 1", "Partial: Training 2", "Zero"])
+  .range(["green","blue","lightblue","orange","lightorange","red"])
 
 var projection = d3.geo.mercator()
     //.center([0, 5])
@@ -18,111 +21,43 @@ var svg = d3.select(".worldmap").append("svg")
     .attr("width", width)
     .attr("height", height)
 
-//This function takes advantage of 2 variables exposed while panning and zooming.
-//d3.event.scale - defines how much the user has zoomed in terms of svg scale
-//d3.event.translate - defines the position of map in relation to the mouse in terms of svg translate
-// var zoomed = function () {
-//   map.attr("transform", "translate("+ d3.event.translate + ")scale(" + d3.event.scale + ")");
-// };
-
-// var zoom = d3.behaviour.zoom()
-//   .scaleExtent([1, 8]) //provies a scale range of the zooming amount
-// //   .on("zoom", zoomed)
-// //   .size([width, height])
-
-// svg.append("rect")
-// .attr("class", "overlay")
-// .attr("width", width)
-// .attr("height", height)
-// .call(zoom);
-
 d3.json("/data/world.json", function(error, world) {
   svg.selectAll(".subunit")
       .data(topojson.feature(world, world.objects.subunits).features)
     .enter().append("path")
       .attr("class", function(d) { return "subunit " + d.id; })
       .attr("d", path)
-
+        .style({
+          stroke:"white",
+          "stroke-width": 0.75,
+          fill: "lightgrey"
+          })
 
     d3.csv("/data/cm.csv", function(data) {
-       var circle = svg.selectAll("circle")
-         .data(data).enter().append("circle")
-         .attr("cx", function(d) {
-                 return projection([d.Longitude, d.Latitude])[0];
-         })
-         .attr("cy", function(d) {
-                 return projection([d.Longitude, d.Latitude])[1];
-         })
-         .attr("r", 5)
-         .on("click", update)
-         .style("fill", function(d,i) { return color(d.Engagement)})
-         // .style("fill", function(d,i) {
-         //  if (d.Engagement === "Full") { return "grey"}
-         //  else if ( d.Engagement === "Partial: CMT 1") { return "blue" }
-         //  else if ( d.Engagement === "Partial: CMT 2") { return "red" }
-         //  else if ( d.Engagement === "Partial: Training 1") { return "purple" }
-         //  else if ( d.Engagement === "Partial: Training 2") { return "pink" }
-         //  else if ( d.Engagement === "Partial: Training 2") { return "orange" }
 
-         //  //else { return "yellow"}
-         // })
-         .style("opacity", 0.75)
+      //currentData will be used for filtering based on legend options. 
+      //var currentData = data//.map(function(d) { return d.Engagement == "*"})
+       var currentData = data//.filter(function(d) { return d.Engagement  === "Full"})
 
-       var tooltip = d3.select("body").append('tooltiptext')
-          .style("position", 'absolute')
-          .style("padding", '10px 10px')
-          .style("background", "lightgrey")
-          .style("opacity", 0)
-       
-
-      var currentcircle;
-
-      function update(d) {
-        if(currentcircle) {
-            currentcircle.transition().duration(2000).attr("r", 5) 
-          }
-        //console.log(d)
-        currentcircle = d3.select(this)
-
-        console.log(d3.select(this))
-        d3.select(this)
-        .style("opacity",1)
-        .classed("selected",true)
-        .transition()
-          .duration(2000)
-          .attr("r", 15)
-
-        tooltip.transition().duration(2000)
-            .style('opacity', .9)
-
-        
-        tooltip.html(function() { 
-          return "<strong>" + "Site: " + "</strong>" + "ABC" + "<br>" + "City: NYC" + "</strong>";
-        })
-         .style('left', (d3.event.pageX + 25) + 'px')
-         .style('top',  (d3.event.pageY - 30) + 'px')
-         .style({ "font-size": "15px", "line-height": "normal"})
-  }//end Update()
+      var legendVals = d3.set(data.map(function(d) { return d.Engagement } )).values()
+      function EngagementVals() {
+        var engaged = d3.set(data.map(function(d) { return d.Engagement } )).values()
+        return engaged
+      }
 
       var legend = svg.selectAll('.legend')
-        //This sorts the legend...took 2 hours to figure this out. 
-        .data(color.domain().slice().sort())
-        //.data(legendKeys, legendKeys["name"])  //below produces no colors and [object Object] for text???
-        //.data(data) //produces colored boxes for all data elements
+        .data(EngagementVals().slice().sort())
         .enter().append('g')
         .attr("class", "legend")
         .attr("transform", function(d,i ) {
           {return "translate(0," + i * 20 + ")"}
         })
 
-      //console.log(legend)
-
       legend.append('rect')
         .attr("x", 110)
         .attr("y", height - 150)
         .attr("width", 10)
         .attr("height", 10)
-        //.on("click", alertme)
         //.style("fill", color)
         .style("fill", color )
 
@@ -133,13 +68,143 @@ d3.json("/data/world.json", function(error, world) {
         .text(function(d,i) { return d})
         .style("text-anchor", "end")
         .style("font-size", 10)
-
-      function alertme() { alert("yup")}
-
+        .on("click", function(d,i) { 
+            var legendChoice = d
+            var newData = (currentData.filter(function(d) { return d.Engagement === legendChoice } ))
+            populateMap(newData)
       
+           })
+
+       
+      populateMap(currentData)
+
+      function populateMap(circledata) {
+        console.log(circledata)
+         var circle = svg.selectAll("circle")
+           .data(circledata)
+
+          circle.attr("class", function(d,i) { 
+              if (d.Engagement === "Full") { return "full"}
+              else if ( d.Engagement === "Partial: CMT 1") { return "partialcmt1" }
+              else if ( d.Engagement === "Partial: CMT 2") { return "partialcmt2" }
+              else if ( d.Engagement === "Partial: Training 1") { return "partialt1" }
+              else if ( d.Engagement === "Partial: Training 2") { return "partialt2" }
+              else if ( d.Engagement === "Zero") { return "zero" }
+            }) 
+            .on("mouseover", mouseover)
+            .on("mouseout", mouseout)
+
+          circle.enter().append("circle")
+           .attr("class", function(d,i) { 
+              if (d.Engagement === "Full") { return "full"}
+              else if ( d.Engagement === "Partial: CMT 1") { return "partialcmt1" }
+              else if ( d.Engagement === "Partial: CMT 2") { return "partialcmt2" }
+              else if ( d.Engagement === "Partial: Training 1") { return "partialt1" }
+              else if ( d.Engagement === "Partial: Training 2") { return "partialt2" }
+              else if ( d.Engagement === "Zero") { return "zero" }
+            }) 
+            .on("mouseover", mouseover)
+            .on("mouseout", mouseout)
+           .attr("cx", function(d) { return projection([d.Longitude, d.Latitude])[0];})
+           .attr("cy", function(d) { return projection([d.Longitude, d.Latitude])[1]; })
+           .attr("r", 5)
+           .on("click", update)
+          //.style("fill", function(d,i) { return color(d.Engagement)})
+          // .style("fill", function(d,i) { return color(d.Engagement)})//this will filter color later on
+           // .style("fill", function(d,i) { 
+           //    if (d.Engagement === "Full") { return "grey"}
+           //    else if ( d.Engagement === "Partial: CMT 1") { return "blue" }
+           //    else if ( d.Engagement === "Partial: CMT 2") { return "red" }
+           //    else if ( d.Engagement === "Partial: Training 1") { return "purple" }
+           //    else if ( d.Engagement === "Partial: Training 2") { return "pink" }
+           //    else if ( d.Engagement === "Zero") { return "orange" }
+           //  })
+           .style("opacity", 0.75)
+
+          circle.exit().remove()
+
+         var tooltip = d3.select("body").append('tooltiptext')
+            .style("position", 'absolute')
+            .style("padding", '10px 10px')
+            .style("background", "white")
+            .style("opacity", 0)
+
+
+
+        function update(d) {
+              if(currentcircle) {
+                  currentcircle.transition().duration(2000).attr("r", 5) 
+                }
+              //console.log(d)
+              currentcircle = d3.select(this)
+
+              //console.log(d3.select(this))
+              d3.select(this)
+              .style("opacity",1)
+              .classed("selected",true)
+              .transition()
+                .duration(2000)
+                .attr("r", 15)
+
+              tooltip.transition().duration(2000)
+                  .style('opacity', .9)
+
+              var string = "";
+                  string = string + "<strong>";
+                  string = string + "Site: " + "</strong>";
+                  string = string + d["Site Code"];
+                  string = string + "<br>";
+                  string = string + "City: "
+                  string = string + d["City"]
+                  string = string + "<hr>"
+                  string = string + "Engagement: "
+                  string = string + d["Engagement"]
+              
+              tooltip.html(function() { return string
+              })
+               .style('left', (d3.event.pageX + 25) + 'px')
+               .style('top',  (d3.event.pageY - 30) + 'px')
+               .style({ "font-size": "15px", "line-height": "normal"})
+               .style({"border": "solid 1px black"})
+        }
+
+         var currentcircle;
+
+      function mouseover(d) {
+         var string = "";
+                  string = string + "<strong>";
+                  string = string + "Site: " + "</strong>";
+                  string = string + d["Site Code"];
+                  string = string + "<br>";
+                  string = string + "City: "
+                  string = string + d["City"]
+                  string = string + "<hr>"
+                  string = string + "Engagement: "
+                  string = string + d["Engagement"]
+
+          tooltip.transition().duration(20)
+            .style('opacity', .9)
+
+          tooltip.html(function() { 
+            return string
+          //return "<strong>" + "Site: " + "</strong>" + d["Site Code"] + "<br>" + "City: " + d["City"] + "</strong>";
+        })
+         .style('left', (d3.event.pageX + 25) + 'px')
+         .style('top',  (d3.event.pageY - 30) + 'px')
+         .style({ "font-size": "15px", "line-height": "normal"})
+        }
+
+      function mouseout(d) {
+        tooltip.transition().duration(20)
+          .style('opacity',0)
+      }
+
+    }
+
+        var currentcircle; 
+    
   });
 });
-
 
 
 
